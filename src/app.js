0d3c0ac9a919403120f3deb14dc7a47c43f48a74
Shaleena Evans <123431776+eshaleena@users.noreply.github.com>
2023-06-10 19:51:46 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -6,14 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  cardGenerator();
+};
+
+let cardGenerator = () => {
   const numbers = ["2", "3", "5", "7", "8"];
   const suit = ["heart", "diamond", "club", "spade"];
-  let random = len => {
-    return Math.floor(Math.random() * len);
-  };
+  let randomSuit = suit[Math.floor(Math.random() * suit.length)];
+  let randomNumbers = numbers[Math.floor(Math.random() * numbers.length)];
 
-  let randomNumber = numbers[random(numbers.length - 1)];
-  let randomSuit = suit[random(suit.length - 1)];
-  document.querySelector(".card").classList.add(".randomNumber");
-  console.log(randomNumber, randomSuit);
+  if (randomSuit == "heart" || randomSuit == "diamond") {
+    document.querySelector("#top-suit").style.color = "red";
+    document.querySelector("#bottom-suit").style.color = "red";
+  } else {
+    document.querySelector("#top-suit").style.color = "black";
+    document.querySelector("#bottom-suit").style.color = "black";
+  }
+  document.querySelector("#top-suit").innerHTML = randomSuit;
+  document.querySelector("#number").innerHTML = randomNumbers;
+  document.querySelector("#bottom-suit").innerHTML = randomSuit;
 };
