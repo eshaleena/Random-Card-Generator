2005db429326ef3ca22c1bc43bcfc5ae4f4c5149
Shaleena Evans <123431776+eshaleena@users.noreply.github.com>
2023-06-10 01:04:32 +0000

    second commit

--- src/app.js
+++ src/app.js
@@ -5,4 +5,15 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {};
+window.onload = function() {
+  const numbers = ["2", "3", "5", "7", "8"];
+  const suit = ["heart", "diamond", "club", "spade"];
+  let random = len => {
+    return Math.floor(Math.random() * len);
+  };
+
+  let randomNumber = numbers[random(numbers.length - 1)];
+  let randomSuit = suit[random(suit.length - 1)];
+  document.querySelector(".card").classList.add(".randomNumber");
+  console.log(randomNumber, randomSuit);
+};
